27bac9f6eb80b2a605f4982aa5bcb60a40fd3e32
Vojta <vojta.hudecek@gmail.com>
2022-10-04 12:43:37 +0200

    universal model change

--- educlf/run.py
+++ educlf/run.py
@@ -11,12 +11,18 @@ import numpy
 
 def get_preprocess_f(label_mapping, tokenizer):
     def fun(example):
-        processed_example = tokenizer(example['text'], padding=True, truncation=True)
-        # processed_example = tokenizer(example['utterance'], padding=True, truncation=True)
-        # processed_example['labels'] = label_mapping[example['labels']]
-        processed_example['labels'] = example['label']
+        processed_example = tokenizer(example['utterance'], padding=True, truncation=True)
+        processed_example['labels'] = label_mapping[example['labels']]
         return processed_example
 
+    return fun
+
+
+def get_preprocess_banking_f(tokenizer):
+    def fun(example):
+        processed_example = tokenizer(example['text'], padding=True, truncation=True)
+        processed_example['labels'] = example['label']
+        return processed_example
     return fun
 
 
@@ -35,22 +41,27 @@ def compute_metrics(predictions):
 
 def main(args):
     device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu:0')
-    train_dataset, dev_dataset = load_data(args.data_fn)
-    all_labels = set(train_dataset['labels'])
+    if args.model in ['robeczech', 'electra']:
+        train_dataset, dev_dataset = load_data(args.data_fn)
+        all_labels = set(train_dataset['labels'])
+        label_mapping = {name: n for n, name in enumerate(all_labels)}
+    else:
+        train_dataset, dev_dataset = datasets.load_dataset('banking77', split=['train', 'test'])
 
-    train_dataset, dev_dataset = datasets.load_dataset('banking77', split=['train', 'test'])
-#    train_dataset = train_dataset.select(range(1000))
-#    dev_dataset = dev_dataset.select(range(500))
-    label_mapping = {name: n for n, name in enumerate(all_labels)}
-    if args.model == 'roberta':
+    if args.model == 'robeczech':
         tokenizer = RobertaTokenizer.from_pretrained('ufal/robeczech-base')
         model = RobertaForSequenceClassification.from_pretrained('ufal/robeczech-base', num_labels=len(all_labels))
+        preprocess_f = get_preprocess_f(label_mapping, tokenizer)
     elif args.model == 'electra':
         tokenizer = ElectraTokenizer.from_pretrained('ufal/eleczech-lc-small')
         model = ElectraForSequenceClassification.from_pretrained('ufal/eleczech-lc-small', num_labels=len(all_labels))
-    else:
+        preprocess_f = get_preprocess_f(label_mapping, tokenizer)
+    elif args.model == 'roberta':
         tokenizer = RobertaTokenizer.from_pretrained('roberta-base', max_length=512)
-        model = RobertaForSequenceClassification.from_pretrained('roberta-base', num_labels=77)
+        model = RobertaForSequenceClassification.from_pretrained('roberta-base', num_labels=77) # 77 is banking77 specific
+        preprocess_f = get_preprocess_banking_f(tokenizer)
+    else:
+        raise ValueError(f'Unknown model name "{args.model}"')
 
     model = model.to(device)
     train_args = TrainingArguments(
@@ -62,8 +73,8 @@ def main(args):
         warmup_steps=200,
         weight_decay=0.01,
     )
-    train_dataset = train_dataset.map(get_preprocess_f(label_mapping, tokenizer))
-    dev_dataset = dev_dataset.map(get_preprocess_f(label_mapping, tokenizer))
+    train_dataset = train_dataset.map(preprocess_f)
+    dev_dataset = dev_dataset.map(preprocess_f)
 
     trainer = Trainer(
         model,
@@ -82,7 +93,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_fn', type=str)
-    parser.add_argument('--model', type=str)
+    parser.add_argument('--model', type=str, default='roberta')
     parser.add_argument('--out_dir', type=str)
     args = parser.parse_args()
     main(args)
