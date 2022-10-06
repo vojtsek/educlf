0a9b1da9bb01432b7d736811a4c43313654c5d6d
Vojta <vojta.hudecek@gmail.com>
2022-10-06 19:32:57 +0200

    added model wrapper, restructured code

---
+++ educlf/model.py
@@ -0,0 +1,142 @@
+import json
+import os.path
+import pickle
+
+import numpy
+import torch
+from transformers import Trainer, TrainingArguments,\
+    RobertaModel, ElectraModel,\
+    RobertaForSequenceClassification, ElectraForSequenceClassification,\
+    RobertaTokenizer, ElectraTokenizer, AutoTokenizer,\
+    AutoModel, EvalPrediction
+
+
+def get_preprocess_f(label_mapping, tokenizer):
+    def fun(example, return_tensors=False):
+        processed_example = tokenizer(example['utterance'],
+                                      padding=True,
+                                      truncation=True,
+                                      return_tensors='pt' if return_tensors else None)
+        if 'labels' in example:
+            processed_example['labels'] = [label_mapping[lbl] for lbl in example['labels']]
+        return processed_example
+    return fun
+
+
+def compute_metrics(predictions):
+    predicted = numpy.argmax(predictions.predictions, axis=-1)
+    acc = numpy.sum((predicted == predictions.label_ids)) / len(predicted)
+    random_label_ids = numpy.random.randint(low=0, high=predictions.label_ids.max(), size=predictions.label_ids.shape)
+    random_acc = numpy.sum((random_label_ids == predictions.label_ids)) / len(predicted)
+    majority_label_ids = numpy.ones(shape=predictions.label_ids.shape) * numpy.argmax(numpy.bincount(predictions.label_ids))
+    majority_acc = numpy.sum((majority_label_ids == predictions.label_ids)) / len(predicted)
+    result = {'accuracy': acc, 'random': random_acc, 'majority': majority_acc}
+    print(result)
+    return result
+
+
+class IntentClassifierModel:
+
+    def __init__(self, model_description, device, label_mapping, out_dir):
+        self._load_mappings(label_mapping)
+        if model_description is not None:
+            model, tokenizer, preprocess_f = self._load_model_from_desc(model_description, device, label_mapping)
+        else:
+            model, tokenizer, preprocess_f = None, None, None
+        self.model_description = model_description
+        self.device = device
+        self.out_dir = out_dir
+        self.model = model
+        self.tokenizer = tokenizer
+        self.preprocess_f = preprocess_f
+        self.trainer = None
+
+    def _load_mappings(self, label_mapping):
+        if label_mapping is not None:
+            self.lbl2id = label_mapping
+            self.id2lbl = {v: k for k, v in label_mapping.items()}
+        else:
+            self.lbl2id, self.id2lbl = None, None
+
+    @staticmethod
+    def _load_model_from_desc(model_description, device, label_mapping):
+        model_cls = AutoModel
+        if model_description == 'robeczech':
+            model_description = 'ufal/robeczech-base'
+            model_cls = RobertaForSequenceClassification
+            tokenizer_cls = RobertaTokenizer
+        elif model_description == 'eleczech':
+            model_description = 'ufal/eleczech-lc-small'
+            model_cls = ElectraForSequenceClassification
+            tokenizer_cls = ElectraTokenizer
+        else:
+            with open(os.path.join(model_description, 'config.json'), 'rt') as fd:
+                cfg = json.load(fd)
+            arch = cfg['architectures'][0]
+            if 'Roberta' in arch:
+                model_cls = RobertaForSequenceClassification
+                tokenizer_cls = RobertaTokenizer
+            else:
+                model_cls = ElectraForSequenceClassification
+                tokenizer_cls = ElectraTokenizer
+        model = model_cls.from_pretrained(model_description, num_labels=len(label_mapping))
+        model = model.to(device)
+        tokenizer = tokenizer_cls.from_pretrained(model_description, max_length=128)
+        preprocess_f = get_preprocess_f(label_mapping, tokenizer)
+        return model, tokenizer, preprocess_f
+
+    def train(self, train_dataset, dev_dataset):
+        if self.out_dir is None:
+            print('Tried to train but out dir is not specified!')
+            return
+        train_dataset = train_dataset.map(self.preprocess_f, batch_size=256, batched=True)
+        dev_dataset = dev_dataset.map(self.preprocess_f, batch_size=256, batched=True)
+        train_args = TrainingArguments(
+            output_dir=self.out_dir,
+            do_eval=True,
+            num_train_epochs=8,
+            evaluation_strategy='epoch',
+            per_device_train_batch_size=8,
+            warmup_steps=200,
+            weight_decay=0.01,
+        )
+        self.trainer = Trainer(
+            self.model,
+            train_args,
+            data_collator=None,
+            train_dataset=train_dataset,
+            eval_dataset=dev_dataset,
+            tokenizer=self.tokenizer,
+            compute_metrics=compute_metrics,
+        )
+        self.trainer.train()
+
+    def predict_from_dataset(self, dataset):
+        if self.trainer is None:
+            print('The model has not been trained yet!')
+            return EvalPrediction([], [])
+        dataset = dataset.map(self.preprocess_f)
+        return self.model.predict(dataset)
+
+    def predict_example(self, example):
+        if not isinstance(example, list):
+            example = [example]
+        example = {'utterance': example}
+        example = self.preprocess_f(example, return_tensors=True)
+        with torch.no_grad():
+            logits = self.model(**example).logits.cpu().numpy()
+        predicted_id = numpy.argmax(logits, axis=-1)
+        return [self.id2lbl[pred] for pred in predicted_id]
+
+    def save(self):
+        self.trainer.save_model(self.out_dir)
+        with open(os.path.join(self.out_dir, 'labels.pkl'), 'wb') as fd:
+            pickle.dump(self.lbl2id, fd)
+
+    def load_from(self, save_dir):
+        with open(os.path.join(save_dir, 'labels.pkl'), 'rb') as fd:
+            label_mapping = pickle.load(fd)
+        self._load_mappings(label_mapping)
+        self.model, self.tokenizer, self.preprocess_f = self._load_model_from_desc(save_dir,
+                                                                                   self.device,
+                                                                                   label_mapping)
--- educlf/run.py
+++ educlf/run.py
@@ -1,43 +1,13 @@
 import argparse
 import datasets
 import torch
-from transformers import Trainer, TrainingArguments,\
-    RobertaForSequenceClassification, RobertaTokenizer,\
-    ElectraForSequenceClassification, ElectraTokenizer
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-import matplotlib.pyplot as plt
-from .dataset import load_data
 import numpy
 
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
-def get_preprocess_f(label_mapping, tokenizer):
-    def fun(example):
-        processed_example = tokenizer(example['utterance'], padding=True, truncation=True)
-        processed_example['labels'] = [label_mapping[lbl] for lbl in example['labels']]
-        return processed_example
-
-    return fun
-
-
-def get_preprocess_banking_f(tokenizer):
-    def fun(example):
-        processed_example = tokenizer(example['text'], padding=True, truncation=True)
-        processed_example['labels'] = example['label']
-        return processed_example
-    return fun
-
-
-def compute_metrics(predictions):
-    predicted = numpy.argmax(predictions.predictions, axis=-1)
-    print(predictions.label_ids, predicted)
-    acc = numpy.sum((predicted == predictions.label_ids)) / len(predicted)
-    random_label_ids = numpy.random.randint(low=0, high=predictions.label_ids.max(), size=predictions.label_ids.shape)
-    random_acc = numpy.sum((random_label_ids == predictions.label_ids)) / len(predicted)
-    majority_label_ids = numpy.ones(shape=predictions.label_ids.shape) * numpy.argmax(numpy.bincount(predictions.label_ids))
-    majority_acc = numpy.sum((majority_label_ids == predictions.label_ids)) / len(predicted)
-    result = {'accuracy': acc, 'random': random_acc, 'majority': majority_acc}
-    print(result)
-    return result
+from .dataset import load_data
+from .model import IntentClassifierModel
 
 
 def main(args):
@@ -50,55 +20,22 @@ def main(args):
     else:
         train_dataset, dev_dataset = datasets.load_dataset('banking77', split=['train', 'test'])
 
-    if args.model == 'robeczech':
-        tokenizer = RobertaTokenizer.from_pretrained('ufal/robeczech-base', max_length=10)
-        model = RobertaForSequenceClassification.from_pretrained('ufal/robeczech-base',
-                                                                 num_labels=len(all_labels))
-        preprocess_f = get_preprocess_f(label_mapping, tokenizer)
-    elif args.model == 'electra':
-        tokenizer = ElectraTokenizer.from_pretrained('ufal/eleczech-lc-small')
-        model = ElectraForSequenceClassification.from_pretrained('ufal/eleczech-lc-small', num_labels=len(all_labels))
-        preprocess_f = get_preprocess_f(label_mapping, tokenizer)
-    elif args.model == 'roberta':
-        tokenizer = RobertaTokenizer.from_pretrained('roberta-base', max_length=512)
-        model = RobertaForSequenceClassification.from_pretrained('roberta-base', num_labels=77) # 77 is banking77 specific
-        preprocess_f = get_preprocess_banking_f(tokenizer)
-    else:
-        raise ValueError(f'Unknown model name "{args.model}"')
+    clf_model = IntentClassifierModel(args.model, device, label_mapping, args.out_dir)
+    clf_model.train(train_dataset, dev_dataset)
+    clf_model.save()
 
-    model = model.to(device)
-    train_args = TrainingArguments(
-        output_dir=args.out_dir,
-        do_eval=True,
-        num_train_epochs=10,
-        evaluation_strategy='epoch',
-        per_device_train_batch_size=8,
-        warmup_steps=200,
-        weight_decay=0.01,
-    )
-    train_dataset = train_dataset.map(preprocess_f, batch_size=256, batched=True)
-    dev_dataset = dev_dataset.map(preprocess_f,  batch_size=256, batched=True)
+    clf_model = IntentClassifierModel(None, device, None, None)
+    clf_model.load_from(args.out_dir)
+    test = clf_model.predict_example('aha no tak zatim zdar')
+    print(test)
 
-    trainer = Trainer(
-        model,
-        train_args,
-        data_collator=None,
-        train_dataset=train_dataset,
-        eval_dataset=dev_dataset,
-        tokenizer=tokenizer,
-        compute_metrics=compute_metrics,
-    )
-    trainer.train()
-    trainer.evaluate(train_dataset)
-    predictions = trainer.predict(dev_dataset)
+    predictions = clf_model.predict_from_dataset(dev_dataset)
     true = [l for l in predictions.label_ids]
     pred = [p for p in numpy.argmax(predictions.predictions, axis=-1)]
     cm = confusion_matrix(true, pred)
     dsp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[inv_label_mapping[i] for i in range(len(inv_label_mapping))])
-#    plt.rcParams["figure.figsize"] = (20,20)
     dsp.plot(xticks_rotation='vertical')
     plt.savefig('cm.png')
-    trainer.save_model()
 
 
 if __name__ == '__main__':
